1e674bfae4bb4e52ca0bfacd974019401efebc79
igor Koniukhov <ikoniukhov@gmail.com>
2022-07-18 12:54:45 +0300

    implemented draw methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod draw;
+mod game;
+
+extern crate piston_window;
+extern crate rand;
+use piston_window::types::Color;
+use piston_window::*;
+
+const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
+
+
 fn main() {
     println!("Hello, world!");
 }
